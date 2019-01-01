0ab020fa652b73dcdeb6f2006b559f04fc4e2aa7
jiangxumin <cjiangxumin@gmail.com>
2019-01-01 17:24:51 +0000

    updaet ...

--- 20/3/main.c
+++ 20/3/main.c
@@ -15,14 +15,21 @@ int main()
 	int nid,ret;
 	EC_GROUP *group = NULL;
 
+	/*
+	size_t EC_get_builtin_curves(EC_builtin_curve *r, size_t nitems)
+	说明: 获取内置的椭圆曲线。
+	      当输入参数 r 为 NULL 或者 nitems 为 0 时，返回内置椭圆曲线的个数，
+	      否则将各个椭圆曲线信息存放在 r 中。
+	*/
 	crv_len = EC_get_builtin_curves(NULL, 0);
 	curves = OPENSSL_malloc(sizeof(EC_builtin_curve) * crv_len); 
-	EC_get_builtin_curves(curves, crv_len);
 
+	/* 获取椭圆曲线列表 */
+	EC_get_builtin_curves(curves, crv_len);
 	for (n=0;n<crv_len;n++) {
 		nid = curves[n].nid; 
 		group=NULL;
-		//printf("nid = %d comment=%s \n", curves[n].nid,curves[n].comment);
+		printf("[%zd] nid = %d comment=%s \n", n , curves[n].nid,curves[n].comment);
 		group = EC_GROUP_new_by_curve_name(nid);
 		ret=EC_GROUP_check(group,NULL); 
 	}
--- 20/4/main.c
+++ 20/4/main.c
@@ -17,7 +17,8 @@ int main()
 	BIO *berr; 
 	EC_builtin_curve *curves;
 	int crv_len; 
-	char shareKey1[128],shareKey2[128];
+	//char shareKey1[128*2],shareKey2[128*2];
+	uint8_t shareKey1[256],shareKey2[256];
 	int len1,len2;
 
 	/* 构造 EC_KEY 数据结构 */ 
@@ -27,11 +28,11 @@ int main()
 		return -1; 
 	}
 	key2=EC_KEY_new(); 
-	if(key2==NULL)
-	{
+	if(key2==NULL) {
 		printf("EC_KEY_new err!\n");
 		return -1;
        	}
+
 	/* 获取实现的椭圆曲线个数 */
 	crv_len = EC_get_builtin_curves(NULL, 0);
 	curves = (EC_builtin_curve *)malloc(sizeof(EC_builtin_curve) * crv_len); 
@@ -41,7 +42,8 @@ int main()
 	nid=curves[0].nid;会有错误，原因是密钥太短
 	*/
 	/* 选取一种椭圆曲线 */
-	nid=curves[25].nid;
+	//nid=curves[25].nid;
+	nid=curves[81].nid; // SM2
 	/* 根据选择的椭圆曲线生成密钥参数 group */ 
 	group1=EC_GROUP_new_by_curve_name(nid);
 	if(group1==NULL) {
@@ -85,14 +87,20 @@ int main()
 	/* 获取密钥大小 */ 
 	size=ECDSA_size(key1); 
 	printf("size %d \n",size); 
-	for(i=0;i<20;i++)
+
+	for(i=0;i<20;i++){
 		memset(&digest[i],i+1,1);
+	}
 
 	signature=malloc(size);
+
+	/*加载各自错误信息*/
 	ERR_load_crypto_strings();
+
 	berr=BIO_new(BIO_s_file());
 	BIO_set_fp(berr,stdout,BIO_NOCLOSE);
 
+	/*签名 验签*/
 	/* 签名数据，本例未做摘要，可将 digest 中的数据看作是 sha1 摘要结果 */ 
 	ret=ECDSA_sign(0,digest,20,signature,&sig_len,key1);
 	if(ret!=1) {
@@ -100,17 +108,27 @@ int main()
 	}
 	/* 验证签名 */ 
 	ret=ECDSA_verify(0,digest,20,signature,sig_len,key1); 
+
 	if(ret!=1) {
 		ERR_print_errors(berr); printf("ECDSA_verify err!\n"); return -1;
 	}
+
+	/** DH 会话密钥**/
 	/* 获取对方公钥，不能直接引用 */
 	pubkey2 = EC_KEY_get0_public_key(key2);
 	/* 生成一方的共享密钥 */
-	len1=ECDH_compute_key(shareKey1, 128, pubkey2, key1, NULL);
+	len1=ECDH_compute_key(shareKey1, 256, pubkey2, key1, NULL);
+	
+	/* printf */
+	for ( i = 0; i<len1; i ++){
+		printf("%02x:",shareKey1[i]);
+	}
+	printf("\n");
+	
 
 	pubkey1 = EC_KEY_get0_public_key(key1);
 	/* 生成另一方共享密钥 */
-	len2=ECDH_compute_key(shareKey2, 128, pubkey1, key2, NULL); 
+	len2=ECDH_compute_key(shareKey2, 256, pubkey1, key2, NULL); 
 	if(len1!=len2) {
 		printf("err\n"); 
 	}
@@ -121,6 +139,7 @@ int main()
 		else
 			printf("生成共享密钥失败\n");
 	}
+
 	printf("test ok!\n"); 
 	BIO_free(berr); 
 	EC_KEY_free(key1); 
