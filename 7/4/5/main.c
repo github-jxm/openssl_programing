814f09bd958497800c441d372b4884cd30a41be3
jiangxumin <cjiangxumin@gmail.com>
2019-01-01 16:21:38 +0000

    update ...

--- 7/4/5/main.c
+++ 7/4/5/main.c
@@ -6,15 +6,17 @@ int main()
 {
 	/* 加密 */ 
 	BIO *bc=NULL,*b=NULL; 
-	const EVP_CIPHER *c=EVP_des_ecb(); 
+	const EVP_CIPHER *c = EVP_des_ecb(); 
 	int len,i;
 	char tmp[1024]={0};
-
 	unsigned char key[8],iv[8];
+
+	/* init key iv */
 	for(i=0;i<8;i++) {
 		memset(&key[i],i+1,1);
-		memset(&iv[i],i+1,1); 
+		memset(&iv[i], i+1,1); 
 	}
+
 	bc=BIO_new(BIO_f_cipher());
 	BIO_set_cipher(bc,c,key,iv,1); 
 	b= BIO_new(BIO_s_null());
@@ -32,6 +34,7 @@ int main()
 	/* 解密 */
 	BIO *bdec=NULL,*bd=NULL;
 	const EVP_CIPHER*cd=EVP_des_ecb();
+
 	bdec=BIO_new(BIO_f_cipher()); 
 	BIO_set_cipher(bdec,cd,key,iv,0); 
 	bd= BIO_new(BIO_s_null()); 
