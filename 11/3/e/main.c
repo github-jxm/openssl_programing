2d83686ea206c91b72ff70f2e42e9e0f734e6a22
jiangxumin <cjiangxumin@gmail.com>
2019-01-18 17:02:03 +0000

    update ...

--- 11/3/e/main.c
+++ 11/3/e/main.c
@@ -23,34 +23,49 @@
 
 #include <openssl/bio.h>
 #include <openssl/bn.h>
+#include <string.h>
 
 int main()
 {
 	BIGNUM *ret1 = NULL, * ret2 = BN_new();
 	char   *p = NULL;
 	int    len = 0;
+	int i;
 	
 	// new bio
 	BIO *bio_out;
 	bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
 
-
-
 	BIO_printf(bio_out,"\n---- BN_bin2bn  ----\n");
        
 	ret1 = BN_bin2bn("242424ab",8, NULL);  /* 二进制 转 bn*/
+
+
+	char arr[1024] = {6};
+	memset(arr,8,100);
+
+	BN_bn2bin(ret1,arr);
+	for ( i = 0; i < 10; i ++){
+	      printf("%02x  " ,arr[i]); 
+	}
+
 	p = BN_bn2hex(ret1);                  /* bn 转 16进制字符串*/
 	//printf("\n0x%s\n",p);
-	BIO_printf(bio_out,"0x%s\n", p);
-	OPENSSL_free(p);  // free
-
-	BIO_printf(bio_out,"\n---- BN_hex2bn / BN_bn2hex ----\n");
-        BIGNUM  * a = BN_new();
-        BN_hex2bn(&a, "ABFE123");   // 将十六进制字符串转换为大数
-	BN_print(bio_out, a);        // 16进制打印
-	p = BN_bn2hex(a);         /* bn 转 16进制字符串*/
 	BIO_printf(bio_out,"\n0x%s\n", p);
 	OPENSSL_free(p);  // free
+
+	int ret = 0;
+	BIO_printf(bio_out,"\n---- BN_hex2bn / BN_bn2hex ----\n");
+        BIGNUM * a = BN_new();
+	//const char * pstr = "ABFE12";
+	const char * pstr = "ABFE12";
+        ret = BN_hex2bn(&a,pstr);   // 将十六进制字符串转换为大数
+	OPENSSL_assert(ret == strlen(pstr));
+	
+	BN_print(bio_out, a);        // 十六进制打印
+	p = BN_bn2hex(a);            // 大数 转 十六进制字符串/
+	BIO_printf(bio_out,"\n0x%s\n", p);
+	OPENSSL_free(p);     // free
 	BN_free(a);
 
 	/*hex to bn*/
